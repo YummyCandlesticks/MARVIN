0aba60a3d7cdf1841b8ce0387e11f75d3d4f80b8
Sarthak Tallamraju <sarthaktallamrajunew@gmail.com>
2024-02-12 22:21:10 +0530

    Inverting the camers
    So that it's easier to track

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ detector = HandDetector(detectionCon=0.8, maxHands=2)
 
 while True:
     success, img = cap.read()
+    img = cv.flip(img, 1)
     hands, img = detector.findHands(img)
     
     if hands: #all the information from the variable is taken from a pre-built dictionary in cvzone, lmList, bbox etc. are all from the package and pre-defined
@@ -17,16 +18,14 @@ while True:
         lmList1 = hand1["lmList"] 
         bbox1 = hand1["bbox"]
         centerPoint1 = hand1["center"] #gives us the centrepoint for the hand
-        handTyp1 = hand1["type"] #gives us the information of whehter this is left or right
-
-        print(len(lmList1), lmList1)
 
     if len(hands)==2: #if there are 2 hands (this is just in case although the previous function can detect both hands we still need the data) allows us to get data for both hands basically
         hand2 = hands[1]
         lmList2 = hand2["lmList"] 
         bbox2 = hand2["bbox"]
         centerPoint2 = hand2["center"] 
-        handTyp2 = hand2["type"] 
+
+    cv.rectangle(img, (100, 100), (300, 300), (255, 0, 255), cv.FILLED)
 
     cv.imshow("Image", img)
     cv.waitKey(1)
