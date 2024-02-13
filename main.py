fbdb66b2b33a1e7506167c65a3e37c96de6ee719
Sarthak Tallamraju <sarthaktallamrajunew@gmail.com>
2024-02-13 21:48:45 +0530

    Added drag functionality by changing the position values of the rectangle to dynamic values

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ cap.set(4, 720)
 detector = HandDetector(detectionCon=0.8, maxHands=2)
 colorR = 255, 0, 255
 
+# instead of defining initial and final points for the rectangles we will define the centre points
+cx, cy, w, h = 100, 100, 200, 200 #instead we are giving the cntrepoints and a fixed width and height
 
 while True:
     success, img = cap.read()
@@ -22,12 +24,16 @@ while True:
 
         if lmList:
             cursor = lmList[8]
-            if 100 < cursor[0] < 300 and 100 < cursor[1] < 300:
+            if cx-w//2 < cursor[0] < cx+w//2 and \
+                cy-h//2 < cursor[1] < cy+h//2:
                 colorR = 0, 255, 0
+                cx, cy = cursor[0], cursor[1]
             else:
                 colorR = 255, 0, 255
 
-    cv.rectangle(img, (100, 100), (300, 300), (colorR), cv.FILLED)
+    
+
+    cv.rectangle(img, (cx-w//2, cy-h//2), (cx+w//2, cy+h//2), (colorR), cv.FILLED) # img, (x1, y1), (x2, y2) initial and final positions
 
     cv.imshow("Image", img)
     cv.waitKey(1)
